994236efdc14182840341280691ba033bca5e143
nassim <nassim.babaci@cloudreach.co.uk>
2016-02-16 00:03:48 -0500

    update todos

--- atlasutils/__main__.py
+++ atlasutils/__main__.py
@@ -12,17 +12,18 @@ def main():
     pass
 
 @main.command()
-@click.option('-s', '--source', required=True,
+@click.option('-s', '--source', required=False,
               default=os.getcwd(),
               type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True),
               help=u"Directory where the git source to publish is")
 @click.option('-n', '--name', required=True, type=click.STRING, help=u"Name of the docker repository to publish to")
-@click.option('-r', '--region', required=True, default='us-east-1',
+@click.option('-r', '--region', required=False, default='us-east-1',
                    help=u"ecr region name")
 @click.option('-t', '--tag', required=False, default=None, help=u"tag")
-@click.option('-f', '--force', required=True, default=False, help=u"Create the repository if it does not exist.")
+@click.option('-f', '--force', required=False, default=False, help=u"Create the repository if it does not exist.")
 def publish_ecr(source, name, region, tag, force):
-    click.secho("/\\tlas <~> 'publish_ecr'", fg='green')
+    click.secho("/\\tlas <~> publish_ecr", fg='green')
+    # TODO: allow source to be a github repo.
     # setup log function.
     registries.log = click.secho
     atlasutils.publish_ecr(source, name, region, tag, force)
